34ed58420a8180a3fcb754aa39185a6ad6a76571
Mahmudul Hasan Sabbir <32410301+SAA33IR@users.noreply.github.com>
2020-06-05 22:57:51 +0600

    Add files via upload

---
+++ IsometricMovementController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class IsometricMovementController : MonoBehaviour
+{
+    [SerializeField] AnimationCurve curveY;
+    Rigidbody2D rb;
+    Vector2 movement;
+    Vector2 currentPos;
+    Vector2 landingPos;
+    float landingDis;
+    float speed = 1f;
+    float timeElapsed = 0f;
+    bool onGround = true;
+    bool jump = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        InputHandler();
+    }
+
+    void FixedUpdate()
+    {
+        if(jump)
+        {
+            JumpHandler();
+        }
+        else
+        {
+            MovementHandler();
+        }
+    }
+    void JumpHandler()
+    {
+        if(onGround)
+        {
+            currentPos = rb.position;
+            landingPos = currentPos + movement.normalized * speed;
+            landingDis = Vector2.Distance(landingPos, currentPos);
+            timeElapsed = 0f;
+            onGround = false;
+        }
+        else
+        {
+            timeElapsed += Time.fixedDeltaTime * speed / landingDis;
+            if(timeElapsed <= 1f)
+            {
+                currentPos = Vector2.MoveTowards(currentPos, landingPos, Time.fixedDeltaTime * speed);
+                rb.MovePosition(new Vector2(currentPos.x, currentPos.y + curveY.Evaluate(timeElapsed)));
+            }
+            else
+            {
+                jump = false;
+                onGround = true;
+            }
+        }
+    }
+
+    void MovementHandler()
+    {
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.fixedDeltaTime);
+    }
+
+    void InputHandler()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        movement = new Vector2(horizontal, vertical);
+
+        if(Input.GetKeyDown("space"))
+        {
+            jump = true;
+        }
+    }
+}
